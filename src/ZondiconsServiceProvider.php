e3cfca115f75b4bf43b53a122bb01711e3901e79
Sam Rapaport <me@samrapdev.com>
2020-04-13 15:24:20 -0700

    Update factory bindings to reference the proper class.

--- src/ZondiconsServiceProvider.php
+++ src/ZondiconsServiceProvider.php
@@ -10,7 +10,7 @@ class ZondiconsServiceProvider extends ServiceProvider
 {
     public function boot()
     {
-        app(IconFactory::class)->registerBladeTag();
+        app(SvgFactory::class)->registerBladeTag();
 
         $this->publishes([
             __DIR__.'/../config/zondicons.php' => config_path('zondicons.php'),
@@ -19,7 +19,7 @@ class ZondiconsServiceProvider extends ServiceProvider
 
     public function register()
     {
-        $this->app->singleton(IconFactory::class, function () {
+        $this->app->singleton(SvgFactory::class, function () {
             $config = array_merge([
                 'icon_path' => base_path('vendor/zondicons/blade-bridge/resources/icons'),
                 'spritesheet_path' => base_path('vendor/zondicons/blade-bridge/resources/sprite.svg'),
